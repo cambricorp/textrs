77ac5dda2b7f1b89703212ee7698ddc3824167d7
chrisaddy <chris.william.addy@gmail.com>
2019-11-25 21:21:22 -0500

    add tokenize function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
+
+pub fn tokenize(text: &str) -> std::vec::Vec<&str> {
+    text.split(" ").collect::<Vec<_>>()
+}
+
+pub fn count(text: &str, delimiter: &str) -> i32 {
+    let result: i32 = text.split(delimiter).next().unwrap_or("0").parse().unwrap_or(0);
+    result
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::tokenize;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_tokenizer() {
+        let t = "hello, this is some text";
+        assert_eq!(tokenize(t), ["hello,", "this", "is", "some", "text"])
     }
+
+    #[test]
+    fn test_tokenizer_single_token() {
+        let t = "hello";
+        assert_eq!(tokenize(t), ["hello"])
+    }
+
+    #[test]
+    fn test_tokenizer_empty() {
+
+        let t = "";
+        assert_eq!(tokenize(t), [""])
+    }
+
+    // #[test]
+    // fn count_simple_text() {
+    //     let t = "hello, this is some text";
+    //     assert_eq!(count(t, " "), 5)
+    // }
+
 }
